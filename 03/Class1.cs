fafc1d304b852f89292512d4df9f7a8a1330b226
Tarek Mohamed <Tarek Mohamed@LAPTOP-2S2UN8OL>
2021-11-27 11:37:28 +0200

    Add project files.

---
+++ 03/Class1.cs
@@ -0,0 +1,49 @@
+﻿///remove by using method///
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Console1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> TekoList = new LinkedList<string>();
+
+            LinkedListNode<string> TekoNode = TekoList.AddFirst("spiderman3");
+            TekoList.AddFirst("spiderman2");
+            TekoList.AddFirst("spiderman1");
+            TekoList.AddBefore(TekoNode, "Venom");
+            TekoList.AddLast("Far From Home");
+
+            Console.WriteLine("The linked list before deletion:  ");
+
+            foreach (var item in TekoList)
+            {
+                Console.Write(item + ",");
+            }
+
+            TekoList.RemoveFirst();
+            TekoList.RemoveLast();
+            TekoList.Remove("Venom");
+            TekoList.Remove("Spiderman3");
+
+            Console.WriteLine();
+            Console.WriteLine("The linked list after deletion: ");
+
+            foreach (var item in TekoList)
+            {
+                Console.Write(item + ",");
+            }
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
+
